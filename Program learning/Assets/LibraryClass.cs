70881cb3cd0e166ed0c319ea0f0a2bb342db111b
frozenwispstudios <32118444+frozenwispstudios@users.noreply.github.com>
2020-07-29 18:29:37 +1200

    Static classes for library classes

---
+++ Program learning/Assets/LibraryClass.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class LibraryClass
+{
+    public static int test()
+    {
+        return 0;
+    }
+
+    //access this from another class by typing 
+    //LibraryClass.test();
+}
