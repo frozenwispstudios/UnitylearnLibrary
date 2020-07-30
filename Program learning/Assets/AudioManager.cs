6e441f6ff8a5f063735bdc70f0eae699b7d0374a
frozenwispstudios <32118444+frozenwispstudios@users.noreply.github.com>
2020-07-30 21:27:35 +1200

    1.1 Audio and gamejam scripts

---
+++ Program learning/Assets/AudioManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine.Audio;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioSource[] sounds;
+    [Range(0f, 1f)]
+    public float volume;
+    [Range(0.1f, 0.5f)]
+    public float pitch;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sounds[0].GetComponent<AudioSource>().Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Program learning/Assets/GamejamLibrary.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GamejamLibrary 
+{
+
+    //get closest object based off tag
+    public static GameObject GetClosestObject(GameObject _self,string _searchObjectsTag)
+    {
+        float distanceToClosestobject = Mathf.Infinity;
+        GameObject closestObject = null;
+        Object[] allObjects = GameObject.FindGameObjectsWithTag(_searchObjectsTag);
+
+        foreach (GameObject currentObject in allObjects)
+        {
+            float distanceToObject = (currentObject.transform.position - _self.transform.position).sqrMagnitude;
+            if (distanceToObject < distanceToClosestobject)
+            {
+                distanceToClosestobject = distanceToObject;
+                closestObject = currentObject;
+            }
+        }
+        return closestObject;
+    }
+
+
+}
